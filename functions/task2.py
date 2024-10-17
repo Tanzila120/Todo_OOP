02c9f53766c60a1203db7a90dd1c9801bdd0c07a
Tanzila120 <t24461524@gmail.com>
2024-10-17 15:17:29 +0000

    Перенос файла task2.py в папку functions

--- functions/task2.py
+++ functions/task2.py
@@ -1,16 +1,19 @@
-import random 
+import random
 
-odd = 0 
-even = 0 
+def is_odd(number):
+    return number % 2 != 0
 
-for i in range(random.randint(1, 100)):
-    total = random.randint(1, 1000)
-    print(total, end=' ')
+def count_odd_even(n):
+    odd_count = 0
+    even_count = 0
+    for _ in range(n):
+        total = random.randint(0, 1000)
+        if is_odd(total):
+            odd_count += 1
+        else:
+            even_count += 1
+    return {"odd": odd_count, "even": even_count}
 
-    if total % 2 == 0: 
-        even += 1 
-    else: 
-        odd += 1 
 
-print('\nЧетные:',even) 
-print('Не четные:', odd)
+result = count_odd_even(100)
+print(result)
